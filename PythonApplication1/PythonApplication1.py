791f118571db13ed0bcb98fa9a3ca4e1af213d7f
Arroyo <adarroyo@FTDCORP.NET>
2018-01-03 18:04:06 -0600

    Add project files.

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,13 @@
+#Variables = Placeholders for data    
+exampleVar = 55 + 32
+print (exampleVar);
+#Variables help programs become more dynamic, and allow a program to always reference a value in one spot.
+#Cannot be named an already existing function or start with a number.
+
+#cannotDo = hey!
+#hey is not a valid data type and this will throw an error, You need quotes to make it valid.
+cando = 'Hey!'
+print(cando);
+#We can store calculations for example
+canContainOperations = 5/4;
+print(canContainOperations);
